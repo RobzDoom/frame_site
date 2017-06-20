8b577a247eebdaf4e8c964625126dd3e45469bc3
robzdoom <jorgecruz.rd@gmail.com>
2017-06-20 16:10:25 -0700

    added nav

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ export default class App extends Component {
   render() {
     return (
       <div className = 'app'>
-        <div>React simple starter</div>
         <Header />
       </div>
     );
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class Header extends Component {
     render (){
         return(
-            <div>This is the footer ok</div>
+            <div>This is the footer ok
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </div>
         )
     };
 }
