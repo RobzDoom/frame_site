2458d34a735ec7f97b5975d2af0d7c9a4df9a82f
robzdoom <jorgecruz.rd@gmail.com>
2017-06-20 15:34:14 -0700

    styling header

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import Header from './header'
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className = 'app'>
         <div>React simple starter</div>
         <Header />
       </div>
