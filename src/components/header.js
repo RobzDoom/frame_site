1b28f9e3df99c2631b6d6f79d992d484460048ec
robzdoom <jorgecruz.rd@gmail.com>
2017-06-26 12:18:31 -0700

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,21 @@ import React, { Component } from 'react'
 class Header extends Component {
     render (){
         return(
-            <div>This is the footer ok
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
+            <div className='header'>This is the footer ok
+                <div className = 'container'>
+                    <div className= 'title'>
+                        <h1><a href="#">Frame Trap</a></h1>
+                    </div>
+                    <div className='nav'>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#news">News</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li><a href="#about">About</a></li>
+                        </ul>
+                    </div>    
+                </div>
+                
             </div>
         )
     };
